548dc3a59e4298fc6fd4ae6d41c5f32b67eabda3
Krzysztof Mataj <krzysztof.mataj@sabre.com>
2020-12-03 18:31:21 +0100

    day2 - different solution

--- day2.hs
+++ day2.hs
@@ -9,10 +9,9 @@ main = do
     contents <- getContents
     let content_lines = lines contents
         input = map splitLine content_lines
-        occurences = solve input
-        occurences' = solve' input
-    print occurences
-    print occurences'
+    print $ solve input
+    print $ solve' input
+    print $ solve'' input
 
 splitLine line = (rangeMin, rangeMax, letter, pass) where
     [rangeMin, rangeMax] = map read . split (== '-') $ rangeStr :: [Int]
@@ -31,3 +30,9 @@ solve' = length . filter isCorrectPass where
     isCorrectPass (index1, index2, letter, pass) = cond1 /= cond2 where
         cond1 = (pass !! (index1 - 1)) == letter
         cond2 = (pass !! (index2 - 1)) == letter
+
+-- Yet another solution. More/less readable?
+solve'' :: [(Int, Int, Char, String)] -> Int
+solve'' = length . filter isCorrectPass where
+    isCorrectPass (index1, index2, letter, pass) = checkIndexes [index1, index2] == 1 where
+        checkIndexes = length . filter (== letter) . map (\n -> pass !! (n-1))
