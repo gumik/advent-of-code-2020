bef1b69418dcb32726c41e948581a5cd4f58427c
Krzysztof Mataj <krzysztof.mataj@sabre.com>
2020-12-03 13:31:55 +0100

    day1 simplifications

--- day1.hs
+++ day1.hs
@@ -1,23 +1,15 @@
-import Control.Monad
 import Data.List
-import Data.Maybe
-import System.Environment
-import System.IO
 import Text.Printf
-import qualified Data.ByteString.Char8 as B
+
+--------------------------------------------------------------------------------
 
 main = do
-    args <- getArgs
-    h <- openFile (head args) ReadMode
-    contents <- B.hGetContents h
-    let splitted = filter (not . B.null) $ B.split '\n' contents
-        numbers = map readInt splitted :: [Int]
+    contents <- getContents
+    let splitted = lines contents
+        numbers = map read splitted :: [Int]
         (num1, num2) = solve numbers
     putStrLn $ show numbers
     printf "%d * %d = %d\n" num1 num2 (num1*num2)
-
-readInt str = number where
-    Just (number, _) = B.readInt str
 
 --------------------------------------------------------------------------------
 
--- day1b.hs
+++ day1b.hs
@@ -1,25 +1,17 @@
-import Control.Monad
 import Data.List
-import Data.Maybe
-import System.Environment
-import System.IO
 import Text.Printf
-import qualified Data.ByteString.Char8 as B
+
+--------------------------------------------------------------------------------
 
 main = do
-    args <- getArgs
-    h <- openFile (head args) ReadMode
-    contents <- B.hGetContents h
-    let splitted = filter (not . B.null) $ B.split '\n' contents
-        numbers = map readInt splitted :: [Int]
+    contents <- getContents
+    let splitted = lines contents
+        numbers = map read splitted
         [num1, num2] = solve 2 numbers
         [num1', num2', num3'] = solve 3 numbers
     putStrLn $ show numbers
     printf "%d * %d = %d\n" num1 num2 (num1*num2)
     printf "%d * %d * %d = %d\n" num1' num2' num3' (num1'*num2'*num3')
-
-readInt str = number where
-    Just (number, _) = B.readInt str
 
 --------------------------------------------------------------------------------
 
@@ -32,3 +24,5 @@ comb :: [Int] -> Int -> [[Int]]
 comb (x:xs) 1 = [x]:comb xs 1
 comb (x:xs) n = map (x:) (comb xs (n-1)) ++ comb xs n
 comb [] _ = []
+
+showResult = intercalate
