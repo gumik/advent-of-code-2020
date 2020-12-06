ed55d9c08dee74d80f05856f94868009ac415b70
Krzysztof Mataj <krzysztof.mataj@sabre.com>
2020-12-06 18:29:21 +0100

    day 5 continuation

--- day5.hs
+++ day5.hs
@@ -1,16 +1,19 @@
+import Data.List
 import Text.Printf
-
---------------------------------------------------------------------------------
 
 main = do
     contents <- getContents
     let input = lines contents
     print $ solve input
+    print $ solve' input
 
---------------------------------------------------------------------------------
+solve = maximum . map convert
 
-solve = maximum . map convert where
-    convert = sum . map (uncurry (*)) . zip coeffs . reverse . map toBin
+solve' input = fst gap + 1 where
+    seats = sort $ map convert input
+    [gap] = filter (\(s1, s2) -> s2 - s1 > 1) $ zip seats (tail seats)
+
+convert = sum . zipWith (*) coeffs . reverse . map toBin where
     coeffs = iterate (*2) 1
     toBin c = case c of
         'R' -> 1
