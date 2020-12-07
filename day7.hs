866afe443f5b909fcade870222d97861aa2c63f5
Krzysztof Mataj <krzysztof.mataj@sabre.com>
2020-12-08 00:05:28 +0100

    day 7, part 1

---
+++ day7.hs
@@ -0,0 +1,26 @@
+import Data.List.Extra
+import Text.Printf
+import qualified Data.Map as M
+import Data.Map ((!), member)
+
+main = do
+    contents <- getContents
+    let input = parse contents
+    print $ solve input
+
+parse = map parseLine . lines where
+    parseLine line = (bag, parseContains containsStr) where
+        [bag, containsStr] = splitOn " bags contain " line
+        parseContains "no other bags." = []
+        parseContains str = map parseQuantityStr $ splitOn ", " str
+        parseQuantityStr str = (read part1 :: Int, part2 ++ " " ++ part3) where
+            part1:part2:part3:_ = splitOn " " str
+
+solve input = length nodesReachedFromShinyGold - 1 where
+    nodesReachedFromShinyGold = nub $ concat $ unfoldr nextNodes ["shiny gold"]
+    nextNodes [] = Nothing
+    nextNodes nodes = Just (nodes, nodes') where
+        leaves = filter (not . (`member` graphMap)) nodes
+        nodes' = concatMap (graphMap !) (nodes \\ leaves)
+    graphMap = M.fromListWith (++) $ concatMap rev input
+    rev (bag, contain) = map (\(n, x) -> (x, [bag])) contain
