9721a2439498e7ad7588fef558aebd8a99bb1f96
Krzysztof Mataj <krzysztof.mataj@sabre.com>
2020-12-14 18:45:35 +0100

    day 13

--- day13.hs
+++ day13.hs
@@ -1,19 +1,32 @@
--- import Data.List.Extra
+import Control.Arrow (second)
+import Data.List.Extra (splitOn)
 import Data.List
-import Text.Printf
 
 main = do
-    --contents <- getContents
-    --let input = parse contents
-    let input = (939, [7,13,59,31,19])
+    contents <- getContents
+    let input = parse contents
     print $ solve input
-    
---parse str = (timestamp, numbers) where
---    [timestamp, numbersStr] = lines str
---    numbers = map read $ filter (/= "x") $ splitOn "," numbersStr :: [Int]
+    print $ solve' $ snd input
 
---solve :: (Int, [Int]) -> [Int]
+parse str = (read timestampStr :: Int, numbers) where
+    [timestampStr, numbersStr] = lines str
+    numbers = map (second read) $ filter ((/= "x") . snd) $ zip [0..] (splitOn "," numbersStr) :: [(Int, Int)]
+
 solve (timestamp, numbers) = (arrival - timestamp) * number where
-    (arrival, number) = head $ sort $ map calc numbers 
+    (arrival, number) = minimum $ map (calc . snd) numbers
     calc x = (if n >= timestamp then n else n + x, x) where
         n = x * (timestamp `div` x)
+
+solve' = fst . foldl1 step
+
+greatestMultipleLowerThan mult n = (n `div` mult) * mult
+lowestMultipleGreaterEqualThan mult n = if m < n then m + mult else m where
+    m = greatestMultipleLowerThan mult n
+
+
+step (start, mult) (pos, num) = (newStart, lcm num mult) where
+    (newStart, _) = head $ dropWhile (\(a, b) -> b - a /= pos) $ baseTimestamps `zip` numTimestamps
+    baseTimestamps = map (timestamp start mult) [0..]
+    numTimestamps = map ((+ greatestMultipleLowerThan num pos) . lowestMultipleGreaterEqualThan num) baseTimestamps
+
+timestamp start mult i = start + (mult * i)
