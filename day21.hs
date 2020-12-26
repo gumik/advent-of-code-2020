c5cf0e3657f25770227b861e122226f3d8ec0b2e
Krzysztof Mataj <gumik0@gmail.com>
2020-12-26 01:36:39 +0100

    day 21, part 2

--- day21.hs
+++ day21.hs
@@ -6,7 +6,8 @@ main = do
     contents <- getContents
     let input = parse contents
     print $ solve input
-
+    putStrLn $ solve' input
+    
 type Ingredient = String
 type Product = [Ingredient]
 type Allergen = String
@@ -18,15 +19,27 @@ parseLine = words >>> break (=="(contains") >>> second (tail >>> map (filter (no
 
 solve input = length $ filter (not . (`elem` ingredientsPossiblyAllergic)) allIngredients where
     allIngredients = concat $ map fst input
-    ingredientsPossiblyAllergic = nub $ concat $ map snd ingredientAllergens
-    ingredientAllergens= map (productsWithAllergen input >>> second possibleIngredientsWithAllergen) (allergens input)
+    ingredientsPossiblyAllergic = nub $ concat $ map snd (ingredientAllergens input)
+
+ingredientAllergens :: Input -> [(Allergen, [Ingredient])]
+ingredientAllergens input = map (productsWithAllergen input >>> second possibleIngredientsWithAllergen) (allergens input)
+
+solve' = intercalate "," . map snd . sort . filterAllergens . ingredientAllergens 
 
 possibleIngredientsWithAllergen :: [Product] -> [Ingredient]
 possibleIngredientsWithAllergen products = calc products where
     calc = concat >>> map (\ing -> (ing,1)) >>> M.fromListWith (+) >>> M.toList >>> filter ((==n).snd) >>> map fst 
     n = length products
+
 productsWithAllergen :: Input -> Allergen -> (Allergen, [Product])
 productsWithAllergen input allergen = (,) allergen $ map fst $ filter (elem allergen . snd) input
 
 allergens :: Input -> [Allergen]
 allergens = nub . concat . map snd
+
+-- Same algorithm as 'filterRules' from day 16
+filterAllergens :: [(Allergen, [Ingredient])] -> [(Allergen, Ingredient)]
+filterAllergens list = if length singles == length list then map (second head) list else filterAllergens reduced where
+    reduced = map (second removeSingles) list
+    singles = concat $ filter ((== 1) . length) $ map snd list
+    removeSingles ingredients = if length ingredients == 1 then ingredients else filter (not . (`elem` singles)) ingredients
