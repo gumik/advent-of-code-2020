8e8266bd8597c482b06ac06790cdfdfea1fb9c91
Krzysztof Mataj <gumik0@gmail.com>
2020-12-29 13:22:13 +0100

    day 23 completed

--- day23.hs
+++ day23.hs
@@ -1,41 +1,27 @@
 import Data.List
 import Data.Maybe
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as M
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as M
 
 main = do
     let input = parse "186524973"
-    putStrLn $ toString $ solve input
+    putStrLn $ solve input
     print $ solve' input
 
 parse :: String -> [Int]
 parse = map (read . (:[]))
 
-solve = rotateAfterOne . last . take 101 . iterate step
+solve = toString . tail . playCups 100
 
-step :: [Int] -> [Int]
-step (h:x1:x2:x3:xs) = put xs [x1,x2,x3] h ++ [h]
+solve' input = product $ take 2 $ tail $ playCups 10000000 (input ++ [10..1000000])
 
-put :: [Int] -> [Int] -> Int -> [Int]
-put list items currItem = take n list ++ items ++ drop n list where
-    n = (fromJust $ findIndex (==(nextLower list currItem)) list) + 1
-    
-nextLower :: [Int] -> Int -> Int
-nextLower list n = head $ dropWhile (not . flip elem list) (map (flip mod 10) [n-1,n-2..])
-
-rotateAfterOne :: [Int] -> [Int]
-rotateAfterOne = tail . uncurry (flip (++)) . break (==1)
-
-toString :: [Int] -> String
-toString = concatMap show
-
-
-solve' input = mapToList 0 $ fst $ last $ take 101 $ iterate (step' (maximum input)) (cupsMap input, head input - 1)
+playCups :: Int -> [Int] -> [Int]
+playCups n input = mapToList 0 $ fst $ last $ take (n+1) $ iterate (step' (maximum input)) (cupsMap input, head input - 1)
 
 cupsMap :: [Int] -> IntMap Int
 cupsMap input = M.fromList $ zip input' (tail input' ++ [head input']) where input' = map (+ (-1)) input
 
---step' :: Int -> (IntMap Int, Int) -> (IntMap Int, Int)
+step' :: Int -> (IntMap Int, Int) -> (IntMap Int, Int)
 step' maxNum (cups, current) = (adjustedCups, next) where
     adjustedCups = M.adjust (const next) current $ M.adjust (const (picked !! 0)) putPosition $  M.adjust (const afterPutPosition) (picked !! 2) cups
     next4 = take 4 $ tail $ nextCups cups current
@@ -52,3 +38,6 @@ nextCups cups = iterate (cups M.!)
 
 mapToList :: Int -> IntMap Int -> [Int]
 mapToList n cups = map(+1) $ take (M.size cups) $ nextCups cups n
+
+toString :: [Int] -> String
+toString = concatMap show
