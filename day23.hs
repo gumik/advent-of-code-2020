c9d8969b756dace45e0ee9cce90f4c600007b27f
Krzysztof Mataj <gumik0@gmail.com>
2020-12-29 09:33:28 +0100

    day 23, work in progress with IntMap

--- day23.hs
+++ day23.hs
@@ -1,15 +1,19 @@
 import Data.List
 import Data.Maybe
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as M
 
 main = do
     let input = parse "186524973"
     putStrLn $ toString $ solve input
+    print $ solve' input
 
 parse :: String -> [Int]
 parse = map (read . (:[]))
 
 solve = rotateAfterOne . last . take 101 . iterate step
 
+step :: [Int] -> [Int]
 step (h:x1:x2:x3:xs) = put xs [x1,x2,x3] h ++ [h]
 
 put :: [Int] -> [Int] -> Int -> [Int]
@@ -21,8 +25,30 @@ nextLower list n = head $ dropWhile (not . flip elem list) (map (flip mod 10) [n
 
 rotateAfterOne :: [Int] -> [Int]
 rotateAfterOne = tail . uncurry (flip (++)) . break (==1)
---rotateAfterOne list = tail p2 ++ p1 ++ [head p2] where
---    (p1, p2) = break (==1) list
 
 toString :: [Int] -> String
 toString = concatMap show
+
+
+solve' input = mapToList 0 $ fst $ last $ take 101 $ iterate (step' (maximum input)) (cupsMap input, head input - 1)
+
+cupsMap :: [Int] -> IntMap Int
+cupsMap input = M.fromList $ zip input' (tail input' ++ [head input']) where input' = map (+ (-1)) input
+
+--step' :: Int -> (IntMap Int, Int) -> (IntMap Int, Int)
+step' maxNum (cups, current) = (adjustedCups, next) where
+    adjustedCups = M.adjust (const next) current $ M.adjust (const (picked !! 0)) putPosition $  M.adjust (const afterPutPosition) (picked !! 2) cups
+    next4 = take 4 $ tail $ nextCups cups current
+    picked = take 3 next4
+    next = last next4
+    putPosition = nextLower' maxNum picked current
+    afterPutPosition = head $ tail $ nextCups cups putPosition
+    
+nextLower' :: Int -> [Int] -> Int -> Int
+nextLower' maxNum justPicked n = head $ dropWhile (`elem` justPicked) (map (`mod` maxNum) [n-1,n-2..])
+
+nextCups :: IntMap Int -> Int -> [Int]
+nextCups cups = iterate (cups M.!)
+
+mapToList :: Int -> IntMap Int -> [Int]
+mapToList n cups = map(+1) $ take (M.size cups) $ nextCups cups n
